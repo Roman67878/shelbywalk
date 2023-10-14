97aab61814402a2572e7939f482775d49f370006
Roman67878 <143877501+Roman67878@users.noreply.github.com>
2023-10-14 14:16:37 +0300

    Update 3.3.py

--- ЯП дз3.py/3.3.py
+++ ЯП дз3.py/3.3.py
@@ -1,10 +1,11 @@
-n = int(input("введите число "))
-
 def fib(n):
     x = [0, 1]
     for i in range(n):
         x.append(x[-1] + x[-2])
     print(x)
+    #print(x[n])
+
+n = int(input("введите число "))
 
 fib(n)
 
