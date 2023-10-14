fa0824d709b01a67094463ef5f0b65e3f4028375
Roman67878 <143877501+Roman67878@users.noreply.github.com>
2023-10-14 14:16:55 +0300

    Update 3.5.py

--- ЯП дз3.py/3.5.py
+++ ЯП дз3.py/3.5.py
@@ -1,11 +1,11 @@
+def average(numb):
+    return sum(numb) / len(numb)
+
 numb = []
 n = int(input("введите желаемую длину списка: "))
 for i in range(1, n+1):
     numb.append(i)
 print(numb)
-
-def average(numb):
-    return sum(numb) / len(numb)
 
 average(numb)
 print(average(numb))
