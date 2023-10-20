47f4261bf3b19ca09405b2a0f51ccf59c134cefc
Roman67878 <143877501+Roman67878@users.noreply.github.com>
2023-10-20 22:19:11 +0300

    test

---
+++ test/test_tree.py
@@ -0,0 +1,7 @@
+from dz4.tree import uniq
+def test_uniq():
+    assert uniq([12, 12, 34, 61, 70])
+    assert uniq('112345')
+  
+
+
---
+++ test/test_two.py
@@ -0,0 +1,3 @@
+from dz4.two import fac
+assert fac(7) == 5040
+assert fac(8) == 40320
