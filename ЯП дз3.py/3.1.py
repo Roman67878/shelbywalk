1566b6b8bfaf625e317112641306f5147a94f5b4
Roman67878 <143877501+Roman67878@users.noreply.github.com>
2023-10-21 12:53:36 +0300

    Update 3.1.py

--- ЯП дз3.py/3.1.py
+++ ЯП дз3.py/3.1.py
@@ -1,10 +1,10 @@
-def spisok(x):
-    y = input("введите число ")
-    while y:
-     x.append(y)
-     y = input("введите число ")
-    print(x)
+def spisok():
+    x = []
+    while True:
+        y = input("введите число ")
+        if not y:
+            break
+        x.append(y)
+    return x
 
-x = []
-
-spisok(x)
+print(spisok())
