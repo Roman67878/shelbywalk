f4ea444ecf08cae287ad56a75c0f0913e8dd9ec0
Roman67878 <143877501+Roman67878@users.noreply.github.com>
2024-03-01 19:25:03 +0300

    Update main.py

--- OOP/main.py
+++ OOP/main.py
@@ -1,18 +1,23 @@
-from Time import Time
+from Time import Time, TimeStamp
 
 
 def main():
-    time = Time()
+    time1 = Time()
+    time2 = TimeStamp()
     while True:
         print("1. Ввод времени")
         print("2. Вывод времени")
-        print("3. Завершить")
+        print("3. Текущее время")
+        print("4. Завершить")
         choise = input("Ваш выбор: ")
         if choise == '1':
-            time.input_time()
+            time1.input_time()
         elif choise == '2':
-            print(time)
-        elif choise == "3":
+            print(time1)
+        elif choise == '3':
+            time2.__init__()
+            time2.__str__()
+        elif choise == '4' or not choise:
             break
 
 
