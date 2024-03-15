95ffe054a10ee3576185d890aad8c58865887936
Roman67878 <143877501+Roman67878@users.noreply.github.com>
2024-03-15 23:22:43 +0300

    Update Time.py

--- OOP/Time.py
+++ OOP/Time.py
@@ -1,4 +1,11 @@
 import datetime
+from math import fabs
+
+
+class TimeValueError(ValueError):
+    def __init__(self, message="", information=""):
+        self.message = message
+        self.information = information
 
 
 class Time:
@@ -8,16 +15,53 @@ class Time:
         self.seconds = seconds
 
     def input_time(self):
-        while True:
-            time_input = input("Введите время в формате Час : Минуты : Секунды: ")
-            self.hours, self.minutes, self.seconds = map(int, time_input.split(":"))
-            if self.hours not in range(24) or self.minutes not in range(60) or self.seconds not in range(60):
-                print("Некорректный ввод")
-            else:
-                break
+        time_input = input("Введите время в формате Час : Минуты : Секунды: ")
+        self.hours, self.minutes, self.seconds = map(int, time_input.split(":"))
+        self.validate()
 
     def __str__(self):
         return f"Время: {self.hours}:{self.minutes}:{self.seconds}"
+
+    def validate(self):
+        if self.hours not in range(24) or self.minutes not in range(60) or self.seconds not in range(60):
+            raise TimeValueError('Неправильные данные')
+
+    def __add__(self, other):
+        sum_time = Time(hours=self.hours + other.hours,
+                        minutes=self.minutes + other.minutes,
+                        seconds=self.seconds + other.seconds)
+        if (self.seconds + other.seconds) >= 60:
+            sum_time.minutes += 1
+            sum_time.seconds -= 60
+
+        if sum_time.minutes >= 60:
+            sum_time.hours += 1
+            sum_time.minutes -= 60
+
+        if sum_time.hours >= 24:
+            raise TimeValueError('Неверный результат сложения')
+
+        return sum_time
+
+    def __sub__(self, other):
+        delta_time = Time(hours=self.hours - other.hours,
+                          minutes=self.minutes - other.minutes,
+                          seconds=self.seconds - other.seconds)
+        if delta_time.seconds < 0:
+            delta_time.minutes -= 1
+            delta_time.seconds += 60
+            if other.seconds > self.seconds:
+                delta_time.seconds = abs(other.seconds - self.seconds)
+
+        if delta_time.minutes < 0:
+            delta_time.hours -= 1
+            delta_time.minutes += 60
+            if self.minutes - other.minutes:
+                delta_time.minutes = abs(other.minutes - self.minutes)
+
+        if delta_time.hours < 0:
+            raise TimeValueError('Неверный результат вычитания')
+        return delta_time
 
 
 class TimeStamp(Time):
@@ -26,6 +70,14 @@ class TimeStamp(Time):
         super().__init__(current.hour, current.minute, current.second)
 
 
-time1 = TimeStamp()
+time1 = Time(12, 23, 34)
+time2 = Time(8, 48, 27)
+time3 = Time(18, 34, 56)
+time4 = Time(17, 43, 43)
+time1 = time1.__add__(time2)
+time3 = time3.__sub__(time4)
 print(time1)
+print(time3)
+time5 = TimeStamp()
+print(time5)
 
